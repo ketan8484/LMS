ab1c543dda2c8772dba69f5f9bfab3926b12f9b3
Kulkarni <ketan.nandkumar-kulkarni@capgemini.com>
2025-06-06 21:32:51 +0530

    Initial commit of LWC project

---
+++ force-app/main/default/lwc/lmsComponentB/lmsComponentB.js
@@ -0,0 +1,20 @@
+import { LightningElement,wire } from 'lwc';
+import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
+import { subscribe,unsubscribe,MessageContext,APPLICATION_SCOPE } from 'lightning/messageService';
+export default class LmsComponentB extends LightningElement {
+    receiveMessage
+    @wire(MessageContext)
+    context
+
+    connectedCallback(){
+        this.subscribeMessage()
+    }
+    subscribeMessage(){
+         subscribe(this.context,SAMPLEMC,(message)=>{this.handleMessage(message)}, {scope:APPLICATION_SCOPE})
+    }
+
+    handleMessage(message){
+        this.receiveMessage = message.lmsData.value? message.lmsData.value : 'No Message pulished'
+    }
+   
+}
