c50977d8cb2e0fc3a76cc9a106c3e2ccf86efe1a
Kulkarni <ketan.nandkumar-kulkarni@capgemini.com>
2025-06-07 23:10:11 +0530

    Initial commit

--- force-app/main/default/lwc/wireDemouserDetail/wireDemouserDetail.js
+++ force-app/main/default/lwc/wireDemouserDetail/wireDemouserDetail.js
@@ -1,10 +1,14 @@
 import { LightningElement, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import id from '@salesforce/user/Id'
+import NAME_FIELD from '@salesforce/schema/user.Name'
+import EMAIL_FIELD from '@salesforce/schema/user.Email'
+const field = [NAME_FIELD,EMAIL_FIELD]
 export default class WireDemouserDetail extends LightningElement {
     userId = id
     userDetail
-    @wire(getRecord, {recordId:'005dM000007rj6DQAQ' , fields:['user.Name', 'user.Email']})
+    //@wire(getRecord, {recordId:'005dM000007rj6DQAQ' , fields:['user.Name', 'user.Email']})
+    @wire(getRecord, {recordId:'005dM000007rj6DQAQ' ,field})
     userDetailHandler({data,error}){
         if(data){
             this.userDetail = data.fields
@@ -14,6 +18,6 @@ export default class WireDemouserDetail extends LightningElement {
         }
     }
 
-    @wire(getRecord, {recordId:'005dM000007rj6DQAQ' , fields:['user.Name', 'user.Email']})
+    @wire(getRecord, {recordId:'005dM000007rj6DQAQ' , field})
     userDetailProperty
 }
